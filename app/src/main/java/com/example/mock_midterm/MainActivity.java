b1b8e5e61dc7eb679e45fc80521f2c9514f28280
Shem <shemcardoza7@gmail.com>
2026-03-11 20:41:25 +0800

    feat: defined variables and basic operations

--- app/src/main/java/com/example/mock_midterm/MainActivity.java
+++ app/src/main/java/com/example/mock_midterm/MainActivity.java
@@ -7,18 +7,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText num1,num2;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        result = findViewById(R.id.result);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void add(View view){
+        int a = Integer.parseInt(num1.getText().toString());
+        int b = Integer.parseInt(num2.getText().toString());
+        result.setText("Result: " + (a + b));
+    }
+
+    public void subtract(View view){
+        int a = Integer.parseInt(num1.getText().toString());
+        int b = Integer.parseInt(num2.getText().toString());
+        result.setText("Result: " + (a - b));
+    }
+
+    public void multiply(View view){
+        double a = Double.parseDouble(num1.getText().toString());
+        double b = Double.parseDouble(num2.getText().toString());
+        result.setText("Result: " + (a * b));
+    }
+
+    public void divide(View view){
+        double a = Double.parseDouble(num1.getText().toString());
+        double b = Double.parseDouble(num2.getText().toString());
+        result.setText("Result: " + (a / b));
+    }
+
+
+
 }
